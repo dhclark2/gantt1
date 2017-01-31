f5799055344b368e6779d2a96b14294cc2431292
Hamilton Clark <dhclark2@ncsu.edu>
2017-01-31 00:04:10 -0500

    no commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :items
   devise_for :users
-  root :to => 'home#index'
+  root :to => 'items#index'
 
   match "home/data", :to => "home#data", :as => "data", :via => "get"
 
@@ -12,5 +12,11 @@ Rails.application.routes.draw do
   post "home/data/link", :to => "link#add"
   put "home/data/link/:id", :to => "link#update"
   delete "home/data/link/:id", :to => "link#delete"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "home/help"
+
+  get "home/about"
+
+  get "home/index"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Task.create :text=>"Task 1", :start_date=>"2015-10-25",  :duration=>2;
-Task.create :text=>"Task 2", :start_date=>"2015-10-27",  :duration=>3;
+
+Task.create :text=>"Check Property Status", :start_date=>"2017-01-29",  :duration=>2;
+Task.create :text=>"Work on App", :start_date=>"2017-01-31",  :duration=>3;
+Task.create :text=>"Get Medicine", :start_date=>"2017-01-25",  :duration=>10;
 Link.create :source=>1, :target=>2, :link_type=>"0";
