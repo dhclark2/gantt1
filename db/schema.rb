0a7d270e5494f8352302d14fa518256fe4008964
Hamilton Clark <dhclark2@ncsu.edu>
2017-01-30 23:03:25 -0500

    added Item scaffold.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131034830) do
+ActiveRecord::Schema.define(version: 20170131040253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "links", force: :cascade do |t|
     t.integer  "source"
