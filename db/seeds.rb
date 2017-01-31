dd61da11f74f5c8815950a5639b083418de1c0f6
Hamilton Clark <dhclark2@ncsu.edu>
2017-01-30 22:45:20 -0500

    gantt chart working.  Basic application.html working.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create :text=>"Task 1", :start_date=>"2015-10-25",  :duration=>2;
+Task.create :text=>"Task 2", :start_date=>"2015-10-27",  :duration=>3;
+Link.create :source=>1, :target=>2, :link_type=>"0";
